b4d2edbc7b143626f35163799f3cccd8e8c807dd
Hassan Shallal <hshallal@gmail.com>
2014-08-24 13:33:32 -0400

    some changes in the interface of the application

--- ui.R
+++ ui.R
@@ -1,16 +1,18 @@
 library(devtools) ## Load devtools
 library(shiny) ## Load shiny
+library(shinyapps) ## Load shinyapps
 shinyUI(pageWithSidebar(
         headerPanel("Calculate Simple or Compounded Interest Rate"),
         sidebarPanel(
-                h4('Enter the following:'),
+                h4('Please enter the values of the prrincipal, interest rate, and time of investment:'),
                 numericInput('input1', 'Principal (USD)', 0, min = 0),
                 numericInput('input2', 'Annual Rate (%. For example: 3.75 for 3.75%)', 0, min = 0, step = 0.00001), 
                 numericInput('input3', 'Time (years)', 0, min = 0),
                 selectInput('type', 'Choose a type of interest', choices = c("simple", "compounded")),
+                h4('Please click submit after entering all the values:'),
                 submitButton('Submit')
         ),
-        mainPanel(
+        sidebarPanel(
                 h4('For principal, you entered:'),
                 verbatimTextOutput("oinput1"),
                 h4('For annual rate, you entered:'),
